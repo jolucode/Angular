60df9a1e88d4030f6ee65ae7bac5bf3729c3cfec
jolubere <jolubere.92@gmail.com>
2024-01-27 13:49:17 -0500

    new

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
 const express = require('express');
-const path = require('path');
+const path = require('');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Angular'));
+app.use(express.static(__dirname + './dist'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
-);
+app.get('*', (req, res) =>{
+    res.sendStatus(200).sendFile(__dirname + 'dist/index.html');
+  });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
